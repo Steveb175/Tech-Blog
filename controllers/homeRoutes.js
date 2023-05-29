e40e6299a262b63aab8bb14c05347a51b28d46bd
Stephen Bondurant <stephenbondurant2014@gmail.com>
2023-05-29 13:00:28 -0500

    Getting closer...

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,12 +1,30 @@
 const router = require("express").Router();
-const { Post } = require("../models");
+const { Post, User } = require("../models");
+const withAuth = require("../utils/auth");
+
+// Home route
+router.get("/", async (req, res) => {
+  try {
+    const dbData = await Post.findAll({
+      attributes: ["title"],
+    });
+    const posts = dbData.map((post) => post.get({ plain: true }));
+    console.log(posts);
+
+    // Render the home.handlebars template and pass the posts data
+    res.render("home", { posts });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to retrieve blog posts" });
+  }
+});
 
 // Dashboard route
 router.get("/dashboard", (req, res) => {
   res.render("dashboard");
 });
 
-router.get("/dashboard", async (req, res) => {
+router.get("/dashboard", withAuth, async (req, res) => {
   try {
     const dbData = await Post.findAll({
       attributes: ["title"],
@@ -22,15 +40,19 @@ router.get("/dashboard", async (req, res) => {
   }
 });
 
-router.get("/", (req, res) => {
-  res.render("home");
-});
-
+// Login route
 router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
   res.render("login");
 });
 
-router.get("/signup", (req, res) => {
+// Signup route
+router.get("/signup", async (req, res) => {
   res.render("signup");
 });
+
 module.exports = router;
